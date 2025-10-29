6b3dbeb392e3b1e5d618cee63f44625edca760d9
Timotius Leonardo <lionato14@gmail.com>
2025-10-30 00:08:46 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,11 +24,6 @@ let package = Package(
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "SDWebImage",
-            url: "https://github.com/SDWebImage/SDWebImage/releases/download/5.21.0/SDWebImage-dynamic.xcframework.zip",
-            checksum: "e034ea04f5e86866bc3081d009941bd5b2a2ed705b3a06336656484514116638"
-        ),
-        .binaryTarget(
             name: "CleverTapSDK",
             url: "https://d1new0xr8otir0.cloudfront.net/CleverTapSDK-7.3.3.xcframework.zip",
             checksum: "9a3a8b7d8118e8d8c5b32744e3d979d051c634d3bddf7542089fb7f3dc3de500"
@@ -51,12 +46,10 @@ let package = Package(
             name: "CleverTapSDKWrapper",
             dependencies: [
                 "CleverTapSDK",
-                "SDWebImage"
             ],
             path: "CleverTapSDKWrapper",
             linkerSettings: [
-                .linkedLibrary("sqlite3"),
-                .linkedFramework("SDWebImage", .when(platforms: [.iOS]))
+                .linkedLibrary("sqlite3")
             ]
         ),
         .target(
